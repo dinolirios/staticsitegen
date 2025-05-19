363a9d335d7f570acd5ce1117100caf089e4445b
dinolirios <thedlirios@gmail.com>
2025-05-19 15:54:46 +0800

    Added Split Nodes Delimiter file and functions, plus tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -33,18 +33,7 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Plain", TextType.TEXT)
         self.assertIsNone(node.url)
 
-    def test_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "This is a text node")
 
-    def test_text2(self):
-        node = TextNode("This is an image", TextType.IMAGE, "facebook.com")
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, 'img')
-        self.assertEqual(html_node.value, "")
-        self.assertEqual(html_node.props, {"src": "facebook.com", "alt": "This is an image"})
 
 if __name__ == "__main__":
     unittest.main()

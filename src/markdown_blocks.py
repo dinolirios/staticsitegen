244e295c77dbb359a3b830b7ba6957efbb09c490
dinolirios <thedlirios@gmail.com>
2025-05-21 15:29:26 +0800

    Added markdown_to_blocks function in markdown_blocks file

---
+++ src/markdown_blocks.py
@@ -0,0 +1,14 @@
+from htmlnode import ParentNode
+from splitnodes_delimiter import text_to_textnodes
+from textnode import *
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+

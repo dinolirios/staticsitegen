1a66e3fa676a57643db89e67a1afa08ee0a5c0c8
dinolirios <thedlirios@gmail.com>
2025-05-22 20:20:18 +0800

    Added extract_title and generate_page functions. Also started generating 8888 site from ./main.sh

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,67 +2,52 @@ class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
-        self.children = children if children is not None else []
-        self.props = props if props is not None else {}
+        self.children = children
+        self.props = props
 
     def to_html(self):
-        raise NotImplementedError("Subclasses should implement this method")
-    
+        raise NotImplementedError("to_html method not implemented")
+
     def props_to_html(self):
-        return " ".join(f'{key}="{value}"' for key, value in self.props.items())
-    
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
     def __repr__(self):
-        return f"HTMLNode({repr(self.tag)}, {self.value}, {repr(self.children)}, {repr(self.props)})"
-    
-    def __eq__(self, other):
-        if not isinstance(other, HTMLNode):
-            return False
-        return (
-            self.tag == other.tag and
-            self.value == other.value and
-            self.children == other.children and
-            self.props == other.props)
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, [], props)
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.value == "" or self.value is None:
-            raise ValueError("LeafNode value cannot be empty")
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
         if self.tag is None:
             return self.value
-        
-        props_html = self.props_to_html()
-        if props_html:
-            return f"<{self.tag} {props_html}>{self.value}</{self.tag}>"
-        else:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
-        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
-        if self.tag == "" or self.tag is None:
-            raise ValueError("ParentNode tag cannot be empty")
-        if self.children == "" or self.children is None:
-            raise ValueError("ParentNode children cannot be empty")
-        return f"<{self.tag}{' ' + self.props_to_html() if self.props else ''}>" + \
-            "".join(child.to_html() for child in self.children) + \
-            f"</{self.tag}>"
-    
-    def test_to_html_with_children(self):
-        child_node = LeafNode("span", "child")
-        parent_node = ParentNode("div", [child_node])
-        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
 
-    def test_to_html_with_grandchildren(self):
-        grandchild_node = LeafNode("b", "grandchild")
-        child_node = ParentNode("span", [grandchild_node])
-        parent_node = ParentNode("div", [child_node])
-        self.assertEqual(
-            parent_node.to_html(),
-            "<div><span><b>grandchild</b></span></div>",
-        )
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
